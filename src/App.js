2db56351ea2f55c1c8cfaafc74c6069fb2afedea
“sadaf <sadaf.batekzai54321@gmial.com>
2023-09-30 14:59:11 +0430

    create react-app and configure the linters

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
+import Rockets from './components/Rockets';
+import Missions from './components/Missions';
+import MyProfile from './components/MyProfile';
+import Navigation from './components/Navigation';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Routes>
+        <Route path="/rockets" element={<Rockets />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/myprofile" element={<MyProfile />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
