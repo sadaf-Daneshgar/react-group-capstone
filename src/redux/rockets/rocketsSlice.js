fb9e07680d172d62bc157bc7da93ff0d5a5f10d4
“sadaf <sadaf.batekzai54321@gmial.com>
2023-10-02 21:10:50 +0430

    Add the rocketSlicer and fetech data from API

--- src/redux/rockets/rocketsSlice.js
+++ src/redux/rockets/rocketsSlice.js
@@ -1,4 +1,12 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const rocketsUrl = 'https://api.spacexdata.com/v4/rockets';
+
+export const fetchRocketsData = createAsyncThunk('rockets/fetchRockets', async () => {
+  const response = await axios.get(rocketsUrl);
+  return response.data;
+});
 
 const initialState = {
   rockets: [],
@@ -10,6 +18,21 @@ const rocketsSlice = createSlice({
   name: 'rockets',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchRocketsData.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(fetchRocketsData.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.rockets = action.payload;
+      })
+      .addCase(fetchRocketsData.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message;
+      });
+  },
 });
 
 export default rocketsSlice.reducer;
