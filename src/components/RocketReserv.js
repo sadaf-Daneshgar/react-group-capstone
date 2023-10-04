b21ae8515ccdf0289cff2c4404df661f124f1e5c
“sadaf <sadaf.batekzai54321@gmial.com>
2023-10-04 23:45:16 +0430

    fix linters error

--- src/components/RocketReserv.js
+++ src/components/RocketReserv.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 function RocketReserv() {
-  const rocketReserve = useSelector((state) =>
-    state.rockets.rockets.filter((rocket) => rocket.reserved === true)
-  );
+  const { rockets } = useSelector((store) => store.rockets);
+  const rocketReserve = rockets.filter((Item) => Item.reserved);
+
   return (
     <>
       <div>
